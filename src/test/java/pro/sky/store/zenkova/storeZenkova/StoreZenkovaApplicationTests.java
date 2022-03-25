e063d2ac634c420f67f122599e8b4da61099e999
AnZenkova <nastyonka16@mail.ru>
2022-03-25 20:06:22 +1000

    Корзина с покупками

---
+++ src/test/java/pro/sky/store/zenkova/storeZenkova/StoreZenkovaApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.store.zenkova.storeZenkova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreZenkovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
